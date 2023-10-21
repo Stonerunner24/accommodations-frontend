635ebb90a49c6b5eb68325aa6ee5368ccaeeff2e
trinajkenyon <trina.kenyon@eagles.oc.edu>
2023-10-21 10:24:55 -0500

    Fixed layouts in routes

--- src/router.js
+++ src/router.js
@@ -1,16 +1,15 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+// Import views
 import Login from "./views/Login.vue";
 import RequestForm from "./views/RequestForm.vue";
-
-/*import TutorialsList from "./views/TutorialsList.vue";
-import EditTutorial from "./views/EditTutorial.vue";
-import AddTutorial from "./views/AddTutorial.vue";
-import ViewTutorial from "./views/ViewTutorial.vue";
-import AddLesson from "./views/AddLesson.vue";
-import EditLesson from "./views/EditLesson.vue"; */
 import AdminHome from "./views/AdminHome.vue";
 import StudentHome from "./views/StudentHome.vue";
+
+// Import layouts
+import SimpleLayout from "./layouts/SimpleLayout.vue";
+import AdminLayout from "./layouts/AdminLayout.vue";
+import StudentLayout from "./layouts/StudentLayout.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,20 +21,37 @@ const router = createRouter({
       component: Login,
     },
     {
-      path: "/studentHome",
-      name: "studentHome",
-      component: StudentHome,
+      // Student is the parent path to all student views and routes
+      // It contains the student header and sidebar in StudentLayout
+      path: "/student",
+      component: StudentLayout,
+      // All student routes are added as children of /student
+      children: [
+        { 
+          path: '', 
+          name: 'studentHome', 
+          component: StudentHome 
+        },
+        {
+          path: "makeRequest",
+          name: "makeRequest",
+          component: RequestForm
+        },
+      ],
     },
     {
-      path: "/adminHome",
-      name: "adminHome",
-      component: AdminHome,
+      path: "/admin",
+      component: AdminLayout,
+      children: [
+        { 
+          path: '', 
+          name: "adminHome", 
+          component: AdminHome
+        },
+      ],
     },
-    {
-      path: "/makeRequest",
-      name: "makeRequest",
-      component: RequestForm,
-    },
+    
+    // Leaving these routes for now as an example
     /*{
       path: "/edit/:id",
       name: "edit",
--- src/views/AdminHome.vue
+++ src/views/AdminHome.vue
@@ -1,11 +1,9 @@
 <script setup>
-import MenuBar from "../components/MenuBar.vue";
-import SideBar from "../components/SideBar.vue";
+
 </script>
 <template>
-    <MenuBar />
-    <SideBar />
+    
     <v-container>
-        <p>This is a test</p>
+        
     </v-container>
 </template>
