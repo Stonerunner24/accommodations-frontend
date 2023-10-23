82846346520579f989c69f0d303f3ecb4c7d9389
trinajkenyon <trina.kenyon@eagles.oc.edu>
2023-10-23 16:17:34 -0500

    Created flattening of data for v-data-table

--- src/views/AdminHome.vue
+++ src/views/AdminHome.vue
@@ -1,13 +1,11 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import RequestServices from '../services/requestServices.js';
-import { computed } from 'vue';
 
 const headers = ref([
-  { text: 'Student ID', value: 'studentId' },
-  { text: 'Name', value: 'name' },
-  { text: 'Email', value: 'email' },
-  { text: 'Date Opened', value: 'dateMade' },
+  { title: 'Student ID', key: 'studentId' },
+  { title: 'Email', key: 'email' },
+  { title: 'Date Opened', key: 'dateMade' },
 ]);
 
 const requests = ref([]);
@@ -15,31 +13,22 @@ const requests = ref([]);
 onMounted(async () => {
   try {
     const response = await RequestServices.getAllForStatus('Open');
-    console.log("Response data: " + response);
+    console.log(response);
     requests.value = response.data;
     console.log("Requests value: " + requests.value);
-    requests.value = requests.value.map(request => {
-      const { fName, lName } = request.student;
-      console.log("inside computed");
-      return {
-        ...request,
-        name: `${fName} ${lName}`
-      };
-    });
+    // flatten the data 
+   
+    const flattenedData = response.data.map(item => ({
+      ...item,
+      email: item.student.email,
+      name: item.student.fName + " " + item.student.lName,
+    })); 
+    requests.value = flattenedData;
+    console.log("Flattened requests");
+    console.log(requests.value);
   } catch (error) {
     console.error(error);
   }
-});
-
-const fullName = computed(() => {
-  return requests.value.map(request => {
-    const { fName, lName } = request.student;
-    console.log("inside computed");
-    return {
-      ...request,
-      name: `${fName} ${lName}`
-    };
-  });
 });
 </script>
 
@@ -47,11 +36,15 @@ const fullName = computed(() => {
     
     <v-container>
         <h1>Open Requests</h1>
-        <v-data-table :headers="headers" :items="requests.value">
-          <template v-slot:items="props">
+        <v-data-table 
+          :headers="headers" 
+          :items="requests.value" 
+          hide-actions 
+          show-select
+        >
+        <template v-slot:items="props">
             <td>{{ props.item.studentId }}</td>
-            <td>{{ props.item.name }}</td>
-            <td>{{ props.item.student.email }}</td>
+            <td>{{ props.item.email }}</td>
             <td>{{ props.item.dateMade }}</td>
           </template>
         </v-data-table>
