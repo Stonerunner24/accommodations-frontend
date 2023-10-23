3ea09cdeaf6ade985afa6e3abcbccc9367f3fe3a
trinajkenyon <trina.kenyon@eagles.oc.edu>
2023-10-23 14:34:36 -0500

    Switch to v-table

--- src/views/AdminHome.vue
+++ src/views/AdminHome.vue
@@ -1,45 +1,19 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import RequestServices from '../services/requestServices.js';
-import { computed } from 'vue';
 
-const headers = ref([
-  { text: 'Student ID', value: 'studentId' },
-  { text: 'Name', value: 'name' },
-  { text: 'Email', value: 'email' },
-  { text: 'Date Opened', value: 'dateMade' },
-]);
-
-const requests = ref([]);
+// Holds all open requests with their associated Student
+const openRequests = ref([]);
 
 onMounted(async () => {
   try {
+    // Get data from API call
     const response = await RequestServices.getAllForStatus('Open');
-    console.log("Response data: " + response);
-    requests.value = response.data;
-    console.log("Requests value: " + requests.value);
-    requests.value = requests.value.map(request => {
-      const { fName, lName } = request.student;
-      console.log("inside computed");
-      return {
-        ...request,
-        name: `${fName} ${lName}`
-      };
-    });
+    // Save in openRequests variable
+    openRequests.value = response.data;
   } catch (error) {
     console.error(error);
   }
-});
-
-const fullName = computed(() => {
-  return requests.value.map(request => {
-    const { fName, lName } = request.student;
-    console.log("inside computed");
-    return {
-      ...request,
-      name: `${fName} ${lName}`
-    };
-  });
 });
 </script>
 
@@ -47,13 +21,34 @@ const fullName = computed(() => {
     
     <v-container>
         <h1>Open Requests</h1>
-        <v-data-table :headers="headers" :items="requests.value">
-          <template v-slot:items="props">
-            <td>{{ props.item.studentId }}</td>
-            <td>{{ props.item.name }}</td>
-            <td>{{ props.item.student.email }}</td>
-            <td>{{ props.item.dateMade }}</td>
-          </template>
-        </v-data-table>
+        <v-table
+          fixed-header
+          height = "20%"
+          >
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Student ID</th>
+              <th>Email</th>
+              <th>Date Opened</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              v-for="request in openRequests"
+              :key="request.studentId">
+              <td>{{ `${request.student.fName} ${request.student.lName}` }}</td>
+              <td>{{ request.studentId }}</td>
+              <td>{{ request.student.email }}</td>
+              <td>{{ request.dateMade }}</td>
+              <td>
+                <v-button>Add Accommodations</v-button>
+              </td>
+            </tr>
+          </tbody>
+        </v-table>
     </v-container>
 </template>
+<style>
+</style>
